36dd5331ef14e97e6d1587f73a5cdb7b1b43292a
karencorneliosbgtv <38926591+karencorneliosbgtv@users.noreply.github.com>
2018-05-02 15:03:46 -0700

    Pool Party - chapter 2

---
+++ SlackExample.java
@@ -0,0 +1,27 @@
+public class SlackExample {
+
+    public static void main (String[], args){
+        Days[] days = new Day[7];
+
+        String beginning = "Its ";
+        String dayName;
+        String conjunction = " the ";
+        String suffix;
+        String ending = " day of the week.";
+
+        for (int i = 0; i < 7; i++){
+            if days[i] = 1{
+
+            }
+
+        }
+
+        System.out.println(beginning + dayName + conjunction + suffix + ending);
+
+    }
+
+}
+
+public class Days{
+
+}
